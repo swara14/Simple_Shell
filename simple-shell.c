3d1d5199bc3990688bca682fc74c5433440a52b1
shrutya22487 <shrutya22487@iiitd.ac.in>
2023-09-05 10:54:18 +0530

    create function files

---
+++ simple-shell.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<readline/readline.h>
+#include<readline/history.h>
+void shell_start(){
+    printf("\n \nSHELL_STARTED\n\n");
+}
