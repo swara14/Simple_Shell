812c9158dd0bdda978415b1ce4cfc99f17ddd273
shrutya22487 <shrutya22487@iiitd.ac.in>
2023-09-05 12:21:27 +0530

    update

--- simple-shell.c
+++ simple-shell.c
@@ -6,6 +6,19 @@
 #include<sys/wait.h>
 #include<readline/readline.h>
 #include<readline/history.h>
-void shell_start(){
-    printf("\n \nSHELL_STARTED\n\n");
+#define clear() printf("\033[H\033[J")
+int main(int argc, char const *argv[])
+{
+    printf("\n\n\n\n******************"
+        "************************");
+    printf("\n\n\n\t****MY SHELL****");
+    printf("\n\n\t-USE AT YOUR OWN RISK-");
+    printf("\n\n\n\n*******************"
+        "***********************");
+    char* username = getenv("USER");
+    printf("\n\n\nUSER is: @%s", username);
+    printf("\n");
+    sleep(3);
+    printf("\033[H\033[J");
+    return 0;
 }
