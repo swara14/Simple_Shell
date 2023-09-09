c6d94ccbbee55a695dfa93c6b3526f07ab619c2b
shrutya22487 <shrutya22487@iiitd.ac.in>
2023-09-09 11:55:10 +0530

    update

--- simple-shell.c
+++ simple-shell.c
@@ -4,208 +4,138 @@
 #include<unistd.h>
 #include<sys/types.h>
 #include<sys/wait.h>
-#include<readline/readline.h>
-#include<readline/history.h>
-#define clear() printf("\033[H\033[J")
-void execArgs(char** parsed)
-{
-    // Forking a child
-    pid_t pid = fork(); 
-  
-    if (pid == -1) {
-        printf("\nFailed forking child..");
-        return;
-    } else if (pid == 0) {
-        if (execvp(parsed[0], parsed) < 0) {
-            printf("\nCould not execute command..\n");
-        }
+#include <stdbool.h>
+#define MAX_HISTORY 100
+#define MAX_COMMAND_LENGTH 100
+
+
+char history[MAX_HISTORY][MAX_COMMAND_LENGTH];
+int pid_history[MAX_HISTORY];
+time_t start_time_history[MAX_HISTORY];
+time_t end_time_history[MAX_HISTORY];
+int count_history = 0;
+
+void add_history(const char *command, int pid, time_t start_time, time_t end_time) {
+    if (count_history < MAX_HISTORY) {
+        strcpy(history[count_history], command);
+        pid_history[count_history] = pid;
+        start_time_history[count_history] = start_time;
+        end_time_history[count_history] = end_time;
+        count_history++;
+    } else {
+        // Handle the case when the history array is full
+        printf("History is full. Cannot add more commands.\n");
+    }
+}
+
+void display_history() {
+    printf("\nCommand History:\n");
+    for (int i = 0; i < count_history; i++) {
+        printf("Command: %s\n", history[i]);
+        printf("PID: %d\n", pid_history[i]);
+        printf("Start Time: %s", ctime(&start_time_history[i]));
+        printf("End Time: %s", ctime(&end_time_history[i]));
+        printf("-------------------------------\n");
+    }
+}
+
+void signal_handler(int signum) {
+    if (signum == SIGINT) {
+        // Ctrl+C was pressed
+        printf("\nCtrl+C received. Displaying command history and program details before exiting.\n");
+        display_history();
+        
+        // Print program details
+        printf("Program PID: %d\n", getpid());
+        time_t current_time = time(NULL);
+        printf("Program Exit Time: %s", ctime(&current_time));
+        
         exit(0);
-    } else {
-        // waiting for child to terminate
-        wait(NULL); 
+    }
+}
+
+void setup_signal_handler() {
+    struct sigaction sa;
+    sa.sa_handler = signal_handler;
+    sigaction(SIGINT, &sa, NULL);
+}
+
+
+void executeCommand(char** argv) {
+    int pid = fork();
+
+    if (pid < 0) {
+        perror("Forking child failed.");
+        exit(1);
+    }
+
+    else if (pid == 0) { //child process
+        execvp(argv[0], argv); // wrong condition ,exec only returns 
+        perror("Command execution failed.");
+        exit(1);
+    }
+
+    else { //parent process
+        int ret;
+        int pid = wait(&ret);
+
+        if (WIFEXITED(ret)) {
+            printf("%d Exit = %d\n",pid,WEXITSTATUS(ret));
+        } else {
+            printf("\nChild process did not exit normally with pid :%d\n" , pid);
+        }
         return;
     }
 }
-// void execArgsPiped(char** parsed, int numPipes) {
-//     if (numPipes <= 0) {
-//         execArgs(parsed); // Execute the last command in the pipeline
-//         return;
-//     }
 
-//     // Create a pipe
-//     int pipefd[2];
-//     if (pipe(pipefd) < 0) {
-//         printf("\nPipe could not be initialized");
-//         return;
-//     }
+void executePipe(char ***commands, int inputfd) {// inputfd is -1 if passing for first time
+    if (commands[1] == NULL) {
+        int pid;
+        pid = fork();
+        if (pid < 0)
+        {
+            perror("Forking child failed.");
+            exit(1);
+        }
+        
+        else if (pid == 0) {
+            if (inputfd != STDIN_FILENO) {
+                dup2(inputfd, STDIN_FILENO);
+                close(inputfd);
+            }
 
-//     pid_t pid = fork();
+            execvp(commands[0][0], commands[0]);
+            exit(0);
+        } 
 
-//     if (pid == -1) {
-//         printf("\nFailed forking child..");
-//         return;
-//     } else if (pid == 0) {
-//         // Child process
-//         close(pipefd[0]); // Close the read end of the pipe
-
-//         // Redirect stdout to the write end of the pipe
-//         dup2(pipefd[1], STDOUT_FILENO);
-//         close(pipefd[1]); // Close the write end of the pipe
-
-//         execArgsPiped(parsed + (numPipes * 2), numPipes - 1); // Recursive call for the next command in the pipeline
-//         exit(0);
-//     } else {
-//         // Parent process
-//         wait(NULL);
-
-//         // Close the write end of the pipe
-//         close(pipefd[1]);
-
-//         // Redirect stdin to the read end of the pipe
-//         dup2(pipefd[0], STDIN_FILENO);
-//         close(pipefd[0]);
-
-//         execArgs(parsed); // Execute the first command in the pipeline
-//     }
-// }
-
-// Function to execute piped system commands with multiple pipes
-void execArgsPiped(char** commands, int numCommands) {
-    if (numCommands <= 0) {
-        // No more commands to execute, return
-        return;
-    }
-
-    int pipefd[2]; // 0 is read end, 1 is write end
-    pid_t pid;
-
-    // Create a pipe
-    if (pipe(pipefd) < 0) {
-        perror("Pipe could not be initialized");
-        return;
-    }
-
-    pid = fork();
-
-    if (pid == -1) {
-        perror("Failed forking child");
-        return;
-    } else if (pid == 0) {
-        // Child process
-
-        // Close the read end of the pipe
-        close(pipefd[0]);
-
-        // Redirect stdout to the write end of the pipe
-        dup2(pipefd[1], STDOUT_FILENO);
-
-        // Close the write end of the pipe
-        close(pipefd[1]);
-
-        // Execute the current command (commands[0])
-        execvp(commands[0], commands);
-
-        // If execvp fails, display an error
-        perror("Could not execute command");
-        exit(EXIT_FAILURE);
-    } else {
-        // Parent process
-
-        // Close the write end of the pipe
-        close(pipefd[1]);
-
-        // Redirect stdin to the read end of the pipe
-        dup2(pipefd[0], STDIN_FILENO);
-
-        // Close the read end of the pipe
-        close(pipefd[0]);
-
-        // Recursively call execArgsPiped for the remaining commands
-        execArgsPiped(commands + 2, numCommands - 1);
-
-        // Wait for the child process to finish
         wait(NULL);
+        return;
     }
+    int fd[2] ,pid;
+    pipe(fd);
+    pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    else if (pid == 0) {
+        if (inputfd != STDIN_FILENO) {
+            dup2(inputfd, STDIN_FILENO);
+            close(inputfd);
+        }
+        close(fd[0]);
+        dup2(fd[1], STDOUT_FILENO);
+
+        execvp(commands[0][0], commands[0]);
+        exit(0);
+    }
+    close(fd[1]);
+    executePipe(++commands, fd[0]);
+    wait(NULL);
+    return;
+    
 }
 
-// You'll need to rearrange your command strings into this three dimensional array
-// of pointers, but by doing so you allow an arbitrary number of commands, each with
-// an arbitrary number of arguments.
-// int executePipe(char ***commands, int inputfd)
-// {
-//     // commands is NULL terminated
-//     if (commands[1] == NULL)
-//     {
-//         // If we get here there's no further commands to execute, so run the 
-//         // current one, and send its result back.
-//         pid_t pid;
-//         int status;
-//         if ((pid = fork()) == 0)
-//         {
-//             // Set up stdin for this process.  Leave stdout alone so output goes to the
-//             // terminal.  If you want '>' / '>>' redirection to work, you'd do that here
-//             if (inputfd != -1)
-//             {
-//                 dup2(inputfd, STDIN_FILENO);
-//                 close(inputfd);
-//             }
-//             execvp(commands[0][0], commands[0]);
-//             perror("execvp");
-//             exit(EXIT_FAILURE);
-//         }
-//         else if (pid < 0)
-//         {
-//             perror("fork");
-//             exit(EXIT_FAILURE);
-//         }
-//         waitpid(pid, &status, 0);
-//         return status;
-//     }
-//     else
-//     {
-//         // Somewhat similar to the above, except we also redirect stdout for the
-//         // next process in the chain
-//         int fds[2];
-//         if (pipe(fds) != 0)
-//         {
-//             perror("pipe");
-//             exit(EXIT_FAILURE);
-//         }
-//         pid_t pid;
-//         int status;
-//         if ((pid = fork()) == 0)
-//         {
-//             // Redirect stdin if needed
-//             if (inputfd != -1)
-//             {
-//                 dup2(inputfd, STDIN_FILENO);
-//                 close(inputfd);
-//             }
-//             dup2(fds[1], STDOUT_FILENO);
-//             close(fds[1]);
-//             execvp(commands[0][0], commands[0]);
-//             perror("execvp");
-//             exit(EXIT_FAILURE);
-//         }
-//         else if (pid < 0)
-//         {
-//             perror("fork");
-//             exit(EXIT_FAILURE);
-//         }
-//         // This is where we handle piped commands.  We've just executed
-//         // commands[0], and we know there's another command in the chain.
-//         // We have everything needed to execute that next command, so call
-//         // ourselves recursively to do the heavy lifting.
-//         status = executePipe(++commands, fds[0]);
-//         // As written, this returns the exit status of the very last command
-//         // in the chain.  If you pass &status as the second parameter here
-//         // to waitpid, you'll get the exit status of the first command.
-//         // It is left as an exercise to the reader to figure how to get the
-//         // the complete list of exit statuses
-//         waitpid(pid, NULL, 0);
-//         return status;
-//     }
-// }
 char** break_pipes_1(char *str) {
     char **commands;
     commands = (char**)malloc(sizeof(char*) * 100);  // Allocate memory for command pointers
@@ -222,6 +152,7 @@ char** break_pipes_1(char *str) {
     commands[i] = NULL;  // Null-terminate the command array
     return commands;
 }
+
 char** break_spaces(char *str) {
     char **command;
     command = (char**)malloc(sizeof(char*) * 100);
@@ -236,24 +167,68 @@ char** break_spaces(char *str) {
     command[i] = NULL;
     return command;
 }
-int main(int argc, char const *argv[]) {
-    printf("\n\nSHELL STARTED\n\n----------------------------\n\n");
-    char *str = "cat helloworld.c | grep print | wc -l";
-    char **command = break_spaces(str);
-    int i = 0;
-    while (command[i] != NULL) {
-        printf("_%s_\n", command[i]);
-        i++;
-    }
 
-    // Free allocated memory
-    i = 0;
-    while (command[i] != NULL) {
-        free(command[i]);
-        i++;
+char*** break_pipes_2( char **str){ // breaks the command we got from the last function on the basis of spaces
+                                    //eg : {"cat helloworld.c" , "grep print" , "wc -l", NULL} is broken into 
+                                    //{{"cat" , "helloworld.c",NULL},{"grep" , "print",NULL}, {"wc" , "-l", NULL} , NULL}
+                                    //this is then passed to executePipe
+    char ***command;
+    command = (char***)malloc(sizeof(char**)*100);
+    int len = 0 ,i = 0;
+    while (str[len] != NULL)
+    {
+        len++;
     }
-    free(command);
+    for (i = 0; i < len; i++)
+    {
+        command[i] = break_spaces( str[i] );
+    }
+    command[i] = NULL;
+    return command;
+
+}
+
+bool check_for_pipes( char* str ){
+    for (int i = 0; i < strlen(str); i++)
+    {
+        if (strcmp(str[i] , "|") == 0 )
+        {
+            return true;
+        }
+        
+    }
+    return false;
+}
+
+int main(int argc, char const *argv[]) {
+    setup_signal_handler(); // Set up the Ctrl+C handler
+
+    char *str;
+    char **command_1;
+    char ***command_2;
+    char c[100] ; // to print the current directory
+    printf("\n\nSHELL STARTED\n\n----------------------------\n\n");
+
+    while (1) {
+        getcwd( c , sizeof(c));
+        printf("Shell>>> %s " , c);
+        str = Input(); // Get user input
+
+        if (check_for_pipes(str)) {
+
+            // If pipes are present, execute piped commands
+
+            command_1 = break_pipes_1(str);     
+            command_2 = break_pipes_2(command_1);
+            executePipe(command_2, -1);
+        } else {
+            // If no pipes, execute a single command
+            command_1 = break_spaces(str);
+            executeCommand(command_1);
+        }
+    }
 
     return 0;
 }
+
 
